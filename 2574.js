2512f893b4f23fdb714ab8badef407e1aaaebb51
mh61503891 <mh.on.web@gmail.com>
2015-12-04 07:19:59 +0900

    hello 2574

---
+++ 2574.js
@@ -0,0 +1,43 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWg2MTUwMzg5MSIsImEiOiJjaWhxbTJjOGMwNGt4dHBtMjczbzhieXZkIn0.J8-B8U-8nCtqiZ2CfxbV0g';
+var position = new mapboxgl.LngLat(133.842941, 35.375086).wrap();
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v8',
+  center: [position.lng, position.lat],
+  zoom: 6
+});
+
+var sourceObj = new mapboxgl.GeoJSONSource({
+  data: 'data/all.geojson'
+});
+
+map.on('style.load', function() {
+  map.addSource("markers", sourceObj);
+  map.addLayer({
+    "id": "markers",
+    "type": "symbol",
+    "source": "markers",
+    "layout": {
+      "icon-image": "{marker-symbol}-15",
+      'icon-optional': true
+    },
+    "paint": {
+      'icon-color': '#0000FF'
+    }
+  });
+});
+
+
+function locate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      lng = position.coords.longitude;
+      lat = position.coords.latitude;
+      var lnglat = new mapboxgl.LngLat(lng, lat).wrap();
+      map.flyTo({
+        center: [lnglat.lng, lnglat.lat],
+        zoom: 15
+      });
+    });
+  }
+};
