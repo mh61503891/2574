0d3193aaf9bc94f1b041b8fd16bd6ef31cf66926
mh61503891 <mh.on.web@gmail.com>
2015-12-04 15:59:58 +0900

    Cleanup

--- web.rb
+++ web.rb
@@ -1,13 +1,7 @@
 require 'sinatra'
 
 set :bind, '0.0.0.0'
-
 set :public_folder, File.dirname(__FILE__) + '/public'
-
 get '/' do
   erb :index
 end
-
-# get '/' do
-#   send_file File.join(settings.public_folder, 'index.html')
-# end
