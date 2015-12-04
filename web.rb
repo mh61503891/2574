48d79ac8b2f767ed22fa017cb91e5210d1a626af
mh61503891 <mh.on.web@gmail.com>
2015-12-05 04:52:55 +0900

    Cleanup

--- web.rb
+++ web.rb
@@ -11,7 +11,6 @@ use Rack::Tracker do
   handler :google_analytics, { tracker: ENV['GOOGLE_TRACKING_ID'] }
 end
 set :bind, '0.0.0.0'
-set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
   erb :index
