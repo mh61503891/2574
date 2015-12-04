dafb9106058457576881840a60ae1657ff48806c
mh61503891 <mh.on.web@gmail.com>
2015-12-04 16:01:08 +0900

    Support Google Analytics

--- web.rb
+++ web.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'rack-google-analytics'
 
+use Rack::GoogleAnalytics, tracker: ENV['GOOGLE_TRACKING_ID']
 set :bind, '0.0.0.0'
 set :public_folder, File.dirname(__FILE__) + '/public'
 get '/' do
